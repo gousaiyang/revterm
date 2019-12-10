355fb10800b7c581ce68faf43e764c6d342c9bcb
gousaiyang <gousaiyang@163.com>
2019-12-09 18:11:21 -0800

    Fix logic in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -x
-cd /usr/local/bin
-sudo rm -f revterm_client revterm_server revterm_killserver revconn_server
+cd /usr/local/bin && sudo rm -f revterm_client revterm_server revterm_killserver revconn_server
